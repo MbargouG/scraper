b79393199ab97a75bb9bdd1ae0eebc0bb63c1f04
MbargouG <97864057+MbargouG@users.noreply.github.com>
2023-04-30 17:39:51 -0500

    update

--- github_scraper.py
+++ github_scraper.py
@@ -7,25 +7,19 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/get_user_info", methods=["POST"])
-def get_user_info():
-    username = request.form["username"]
+@app.route("/search_users")
+def search_users():
+    query = request.args.get('query')
     # Replace YOUR_GITHUB_TOKEN with your own GitHub API token
-    headers = {'Authorization': 'token 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'}
-    # Make a request for user information based on the username
-    response = requests.get(f'https://api.github.com/users/{username}', headers=headers)
+    headers = {'Authorization': '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'}
+    # Make a request for searching users based on the query
+    response = requests.get(f'https://api.github.com/search/users?q={query}', headers=headers)
     if response.status_code == 200:
         # Parse and extract the relevant data
-        user_info = response.json()
-        name = user_info['name']
-        avatar_url = user_info['avatar_url']
-        bio = user_info['bio']
-        public_repos = user_info['public_repos']
-        followers = user_info['followers']
-        following = user_info['following']
-        return render_template("user_info.html", name=name, avatar_url=avatar_url, bio=bio, public_repos=public_repos, followers=followers, following=following)
+        search_results = response.json()['items']
+        return render_template("search_results.html", search_results=search_results)
     else:
-        return "Failed to get user information"
+        return "Failed to search for users"
 
 if __name__ == '__main__':
     app.run(debug=True)
