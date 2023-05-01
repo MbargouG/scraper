767b09eb40cfd899e63e22e1b94e0ec9ed4382cb
MbargouG <97864057+MbargouG@users.noreply.github.com>
2023-04-30 19:45:33 -0500

    change

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import requests
+import json
 from flask import Flask, render_template, request
 import requests
 
@@ -9,17 +11,22 @@ def index():
 
 @app.route("/get_user_info", methods=["POST"])
 def get_user_info():
-    username = request.form["username"]
-    # Replace YOUR_GITHUB_TOKEN with your own GitHub API token
-    headers = {"Authorization": "token YOUR_GITHUB_TOKEN"}
-    # Make a request for the user's profile data
-    response = requests.get(f"https://api.github.com/users/{username}", headers=headers)
-    if response.status_code == 200:
-        # Parse and extract the relevant data
-        user_info = response.json()
-        return render_template("user_info.html", user_info=user_info)
-    else:
-        return "Failed to get user info"
+    # Get the username entered by the user
+username = request.form['username']
+
+# Replace YOUR_GITHUB_TOKEN with your own GitHub API token
+headers = {'Authorization': 'token YOUR_GITHUB_TOKEN'}
+
+# Make a request for the user information
+response = requests.get(f'https://api.github.com/users/{username}', headers=headers)
+
+if response.status_code == 200:
+    # Parse the response JSON
+    user_info = json.loads(response.text)
+    return render_template("user_info.html", user_info=user_info)
+else:
+    return "Failed to retrieve user information"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
